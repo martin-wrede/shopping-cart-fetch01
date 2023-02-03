ea3bee5e61668ab2874a423e1ceeafad8fa5b7ff
martin-wrede <81897938+martin-wrede@users.noreply.github.com>
2023-02-03 13:18:19 +0100

    first commit

---
+++ vite-project/src/Photos.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+function Photos() {
+    return (
+      <div>
+        <h1>This is the photo page</h1>
+      
+      </div>
+    );
+  }
+  
+  export default Photos;
